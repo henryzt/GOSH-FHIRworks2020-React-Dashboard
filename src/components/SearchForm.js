a33cbebccc46f54c964463c7831d0d14ca9ceb37
Henry Zhang <henryzhang9802@gmail.com>
2020-03-03 21:51:49 +0000

    Update style

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -23,17 +23,17 @@ const SearchForm = props => {
       onFinish={onFinish}
     >
       <Row gutter={24}>
-        <Col xs={23} sm={23} md={12} lg={8} className="search_inputs" key={1}>
+        <Col sm={24} md={12} lg={8} className="search_inputs" key={1}>
           <Form.Item name={`name`} label={`Patient Name`}>
             <Input placeholder="Enter Patient's name to filter" />
           </Form.Item>
         </Col>
-        <Col xs={23} sm={23} md={12} lg={8} className="search_inputs" key={2}>
-          <Form.Item name="birth-date" label="Birthdate Range">
+        <Col sm={24} md={12} lg={8} className="search_inputs" key={2}>
+          <Form.Item name="birthdate" label="Birthdate Range">
             <RangePicker />
           </Form.Item>
         </Col>
-        <Col xs={23} sm={23} md={12} lg={8} className="search_inputs" key={3}>
+        <Col sm={24} md={12} lg={8} className="search_inputs" key={3}>
           <Form.Item name="gender" label="Gender">
             <Select placeholder="Select a gender to filter" allowClear>
               <Option value="male">male</Option>
@@ -42,36 +42,32 @@ const SearchForm = props => {
             </Select>
           </Form.Item>
         </Col>
-      </Row>
 
-      <Row gutter={24}>
-        <Col xs={23} sm={23} md={12} lg={8} className="search_inputs" key={1}>
+        <Col sm={24} md={12} lg={8} className="search_inputs" key={4}>
           <Form.Item name={`phone`} label={`Phone number`}>
             <Input placeholder="Enter phone number to filter" />
           </Form.Item>
         </Col>
 
-        <Col xs={23} sm={23} md={12} lg={8} className="search_inputs" key={2}>
+        <Col sm={24} md={12} lg={8} className="search_inputs" key={5}>
           <Form.Item name={`address`} label={`Country and Address`}>
             <Input placeholder="Enter counrty code to filter" />
           </Form.Item>
         </Col>
 
-        <Col xs={23} sm={23} md={12} lg={8} className="search_inputs" key={2}>
+        <Col sm={24} md={12} lg={8} className="search_inputs" key={6}>
           <Form.Item name={`maritalStatus`} label={`Marital Status`}>
             <Input placeholder="Enter marital status to filter" />
           </Form.Item>
         </Col>
-      </Row>
 
-      <Row gutter={24}>
-        <Col xs={23} sm={23} md={12} lg={8} className="search_inputs" key={1}>
+        <Col sm={24} md={12} lg={8} className="search_inputs" key={7}>
           <Form.Item name={`id`} label={`Patient ID`}>
             <Input placeholder="Enter Patient ID to filter" />
           </Form.Item>
         </Col>
-        <Col span={16}>
-          <Form.Item name={["user", "anything"]} label="Search for anything else">
+        <Col sm={24} md={23} lg={16} span={16} key={8}>
+          <Form.Item name={"anythingElse"} label="Search for anything else">
             <Input.TextArea placeholder="Just type anything you would like to search, you can search for Social Security Number, Driver's License, Passport Number, Ethics etc. Regular expression is supported." />
           </Form.Item>
         </Col>
