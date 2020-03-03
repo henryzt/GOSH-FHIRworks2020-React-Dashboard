35745ae0023fd3ba06f09200b90dcf416dfbfffd
Henry Zhang <henryzhang9802@gmail.com>
2020-03-03 21:26:08 +0000

    Add more search fields

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import { Form, Row, Col, Input, Button, DatePicker } from "antd";
+import { Form, Row, Col, Input, Button, DatePicker, Select } from "antd";
 import { DownOutlined, UpOutlined } from "@ant-design/icons";
+
 const { RangePicker } = DatePicker;
+const { Option } = Select;
 
 const SearchForm = props => {
   const [expand, setExpand] = useState(false);
@@ -22,16 +24,59 @@ const SearchForm = props => {
     >
       <Row gutter={24}>
         <Col span={8} key={1}>
-          <Form.Item name={`name`} label={`Name`}>
-            <Input placeholder="placeholder" />
+          <Form.Item name={`name`} label={`Patient Name`}>
+            <Input placeholder="Enter Patient's name to filter" />
           </Form.Item>
         </Col>
         <Col span={8} key={2}>
-          <Form.Item name="range-picker" label="Birthdate">
+          <Form.Item name="birth-date" label="Birthdate Range">
             <RangePicker />
           </Form.Item>
         </Col>
+        <Col span={8} key={3}>
+          <Form.Item name="gender" label="Gender">
+            <Select placeholder="Select a gender to filter" allowClear>
+              <Option value="male">male</Option>
+              <Option value="female">female</Option>
+              <Option value="other">other</Option>
+            </Select>
+          </Form.Item>
+        </Col>
       </Row>
+
+      <Row gutter={24}>
+        <Col span={8} key={1}>
+          <Form.Item name={`phone`} label={`Phone number`}>
+            <Input placeholder="Enter phone number to filter" />
+          </Form.Item>
+        </Col>
+
+        <Col span={8} key={2}>
+          <Form.Item name={`address`} label={`Country and Address`}>
+            <Input placeholder="Enter counrty code to filter" />
+          </Form.Item>
+        </Col>
+
+        <Col span={8} key={2}>
+          <Form.Item name={`maritalStatus`} label={`Marital Status`}>
+            <Input placeholder="Enter marital status to filter" />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row gutter={24}>
+        <Col span={8} key={1}>
+          <Form.Item name={`id`} label={`Patient ID`}>
+            <Input placeholder="Enter Patient ID to filter" />
+          </Form.Item>
+        </Col>
+        <Col span={16}>
+          <Form.Item name={["user", "anything"]} label="Search for anything else">
+            <Input.TextArea placeholder="Just type anything you would like to search, you can search for Social Security Number, Driver's License, Passport Number, Ethics etc." />
+          </Form.Item>
+        </Col>
+      </Row>
+
       <Row>
         <Col
           span={24}
