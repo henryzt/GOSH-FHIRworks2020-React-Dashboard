239979e0c213b749e4dbcf31cc2f537bc208b21b
Henry Zhang <henryzhang9802@gmail.com>
2020-03-03 14:10:07 +0000

    Add home page css

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import "./home.css";
 import { Layout, message, Pagination, Row, Col } from "antd";
 
 class HomePage extends React.Component {
   render() {
-    return <div>HomePage</div>;
+    return (
+      <div>
+        <div className="center">FHIR Dashboard</div>
+        <div className="center_float">
+          <div className="menu_card">Patients</div>
+          <div className="menu_card">Patients</div>
+          <div className="menu_card">Patients</div>
+        </div>
+      </div>
+    );
   }
 }
 
