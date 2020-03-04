093123d47dc68a526922949b7cc6a5950760115b
Henry Zhang <henryzhang9802@gmail.com>
2020-03-04 23:56:21 +0000

    Bug fixes

--- src/components/ObservationDrawer.js
+++ src/components/ObservationDrawer.js
@@ -2,6 +2,13 @@ import React from "react";
 import { requestObservation } from "../javascript/api";
 import { Drawer, Descriptions, message } from "antd";
 
+const keyGen = () => {
+  let r = Math.random()
+    .toString(36)
+    .substring(7);
+  return r;
+};
+
 class ObservationDrawer extends React.Component {
   constructor(props) {
     super(props);
@@ -36,27 +43,34 @@ class ObservationDrawer extends React.Component {
   render() {
     const { visible } = this.props;
     const patient = this.props.patient && this.props.patient.resource;
-    let key = 0;
 
-    const observations =
+    let observations =
       this.state.observation &&
       this.state.observation.map(entry => {
         let obs = entry.resource;
         return (
-          <Descriptions key={key++} title={obs.code.text}>
-            <Descriptions.Item key={key++} label="ID">
-              {obs.id}
-            </Descriptions.Item>
-            <Descriptions.Item key={key++} label="Category">
-              {obs.category[0].coding[0].display}
-            </Descriptions.Item>
-            <Descriptions.Item key={key++} label="effectiveDateTime">
-              {obs.effectiveDateTime}
-            </Descriptions.Item>
-            <Descriptions.Item key={key++} label="issued">
-              {obs.issued}
-            </Descriptions.Item>
-          </Descriptions>
+          <div key={keyGen()} style={{ width: "100%" }}>
+            <Descriptions
+              bordered={true}
+              layout="vertical"
+              key={keyGen()}
+              title={obs.code.text}
+              column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
+            >
+              <Descriptions.Item key={keyGen()} label="ID">
+                {obs.id}
+              </Descriptions.Item>
+              <Descriptions.Item key={keyGen()} label="Category">
+                {obs.category?.[0]?.coding?.[0].display}
+              </Descriptions.Item>
+              <Descriptions.Item key={keyGen()} label="effectiveDateTime">
+                {obs.effectiveDateTime}
+              </Descriptions.Item>
+              <Descriptions.Item key={keyGen()} label="issued">
+                {obs.issued}
+              </Descriptions.Item>
+            </Descriptions>
+          </div>
         );
       });
 
@@ -70,21 +84,21 @@ class ObservationDrawer extends React.Component {
         width={"40%"}
       >
         {patient && (
-          <div>
+          <div key={keyGen()}>
             <Descriptions title="Patient Basic Info">
-              <Descriptions.Item key={key++} label="Name">
-                {`${patient.name[0].family} ${patient.name[0].given[0]} (${patient.name[0].prefix[0]})`}
+              <Descriptions.Item key={keyGen()} label="Name">
+                {`${patient.name[0]?.family} ${patient.name[0]?.given?.[0]} (${patient.name[0]?.prefix?.[0]})`}
               </Descriptions.Item>
-              <Descriptions.Item key={key++} label="ID">
+              <Descriptions.Item key={keyGen()} label="ID">
                 {patient.id}
               </Descriptions.Item>
-              <Descriptions.Item key={key++} label="Telephone">
+              <Descriptions.Item key={keyGen()} label="Telephone">
                 {patient.telecom[0].value}
               </Descriptions.Item>
-              <Descriptions.Item key={key++} label="Birth Date">
+              <Descriptions.Item key={keyGen()} label="Birth Date">
                 {patient.birthDate}
               </Descriptions.Item>
-              <Descriptions.Item key={key++} label="Address">
+              <Descriptions.Item key={keyGen()} label="Address">
                 {`${patient.address[0].line[0]}, ${patient.address[0].city}, ${patient.address[0].state}, ${patient.address[0].country}`}
               </Descriptions.Item>
             </Descriptions>
