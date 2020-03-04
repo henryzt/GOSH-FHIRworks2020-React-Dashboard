ebc05ccaa78bf6562903635d227425aa9d78f7b7
Henry Zhang <henryzhang9802@gmail.com>
2020-03-04 01:35:45 +0000

    Add local cache

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -9,14 +9,22 @@ function combinePatientsBundle(json) {
 
 function request() {
   return new Promise((resolve, reject) => {
-    fetch("http://178.62.0.181:5000/api/Patient/")
-      .then(async res => {
-        let json = await res.json();
-        console.log(json);
-        json = combinePatientsBundle(json);
-        resolve(json);
-      })
-      .catch(console.log);
+    let localCache = localStorage.getItem("patients");
+    if (localCache) {
+      setTimeout(() => {
+        resolve(JSON.parse(localCache));
+      }, 1000);
+    } else {
+      fetch("http://178.62.0.181:5000/api/Patient/")
+        .then(async res => {
+          let json = await res.json();
+          console.log(json);
+          json = combinePatientsBundle(json);
+          localStorage.setItem("patients", JSON.stringify(json));
+          resolve(json);
+        })
+        .catch(console.log);
+    }
   });
 }
 
