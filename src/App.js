bd1619e3b393f239833792d5dc3c0ba37b43213f
Henry Zhang <henryzhang9802@gmail.com>
2020-03-04 20:59:06 +0000

    Update transition structure

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from "./routes/home";
 import SideMenu from "./components/SideMenu";
 
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { BrowserRouter as Router, Switch, Route, Link, withRouter } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, useLocation } from "react-router-dom";
 
 import { Layout } from "antd";
 
@@ -43,65 +43,72 @@ class App extends React.Component {
 
   render() {
     console.log(this.props);
-    let { location } = this.props;
     return (
-      <Layout style={{ minHeight: 100 + "vh" }}>
-        <Sider
-          collapsible
-          breakpoint="lg"
-          width="230"
-          style={{
-            boxShadow: "7px 0px 20px -10px rgba(0,0,0,0.35)",
-            overflow: "auto",
-            height: "100vh",
-            position: "sticky",
-            top: 0,
-            left: 0
-          }}
-        >
-          <div className="logo">
-            <h2 style={{ color: "white" }}>FHIR Dashboard</h2>
-          </div>
-          <SideMenu></SideMenu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header
-            className="site-layout-background"
-            style={{ padding: 0, boxShadow: "0px 6px 20px -10px rgba(0,0,0,0.05)", zIndex: 20 }}
+      <Router>
+        <Layout style={{ minHeight: 100 + "vh" }}>
+          <Sider
+            collapsible
+            breakpoint="lg"
+            width="230"
+            style={{
+              boxShadow: "7px 0px 20px -10px rgba(0,0,0,0.35)",
+              overflow: "auto",
+              height: "100vh",
+              position: "sticky",
+              top: 0,
+              left: 0
+            }}
           >
-            <h2 style={{ paddingLeft: 20 + "px" }}>
-              <Switch location={location}>
-                {routes.map((route, index) => (
-                  <Route
-                    key={index}
-                    path={route.path}
-                    exact={route.exact}
-                    children={<route.title />}
-                  />
-                ))}
-              </Switch>
-            </h2>
-          </Header>
-          <TransitionGroup>
-            <CSSTransition key={location.key} classNames="fade" timeout={{ enter: 300, exit: 300 }}>
-              <Switch location={location}>
-                {routes.map((route, index) => (
-                  <Route
-                    key={index}
-                    path={route.path}
-                    exact={route.exact}
-                    children={<route.main />}
-                  />
-                ))}
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
+            <div className="logo">
+              <h2 style={{ color: "white" }}>FHIR Dashboard</h2>
+            </div>
+            <SideMenu></SideMenu>
+          </Sider>
+          <Layout className="site-layout">
+            <Header
+              className="site-layout-background"
+              style={{ padding: 0, boxShadow: "0px 6px 20px -10px rgba(0,0,0,0.05)", zIndex: 20 }}
+            >
+              <h2 style={{ paddingLeft: 20 + "px" }}>
+                <Switch>
+                  {routes.map((route, index) => (
+                    <Route
+                      key={index}
+                      path={route.path}
+                      exact={route.exact}
+                      children={<route.title />}
+                    />
+                  ))}
+                </Switch>
+              </h2>
+            </Header>
 
-          <Footer style={{ textAlign: "center" }}>FHIR Dashboard ©2020 Created by henryz00</Footer>
+            <RouterContent></RouterContent>
+
+            <Footer style={{ textAlign: "center" }}>
+              FHIR Dashboard ©2020 Created by henryz00
+            </Footer>
+          </Layout>
         </Layout>
-      </Layout>
+      </Router>
     );
   }
 }
 
-export default withRouter(App);
+const RouterContent = () => {
+  let location = useLocation();
+  console.log(location);
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={300}>
+        <Switch location={location}>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} exact={route.exact} children={<route.main />} />
+          ))}
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
+export default App;
