6a79374d50fe2e0d9d41240312311c9f20e98116
Henry Zhang <henryzhang9802@gmail.com>
2020-03-04 22:03:20 +0000

    Add patient info

--- src/components/ObservationDrawer.js
+++ src/components/ObservationDrawer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Drawer } from "antd";
+import { Drawer, Descriptions } from "antd";
 
 class ObservationDrawer extends React.Component {
   constructor(props) {
@@ -15,7 +15,10 @@ class ObservationDrawer extends React.Component {
   };
 
   render() {
-    const { visible, patient } = this.props;
+    const { visible } = this.props;
+    const patient = this.props.patient && this.props.patient.resource;
+    console.log(patient);
+    let key = 0;
     return (
       <Drawer
         title="Patient Observation"
@@ -23,10 +26,27 @@ class ObservationDrawer extends React.Component {
         closable={true}
         onClose={this.onClose}
         visible={visible}
+        width={"40%"}
       >
-        <p>{patient && patient.resource.name[0].family}</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        {patient && (
+          <Descriptions title="Patient Basic Info">
+            <Descriptions.Item key={key++} label="Name">
+              {`${patient.name[0].family} ${patient.name[0].given[0]} (${patient.name[0].prefix[0]})`}
+            </Descriptions.Item>
+            <Descriptions.Item key={key++} label="ID">
+              {patient.id}
+            </Descriptions.Item>
+            <Descriptions.Item key={key++} label="Telephone">
+              {patient.telecom[0].value}
+            </Descriptions.Item>
+            <Descriptions.Item key={key++} label="Birth Date">
+              {patient.birthDate}
+            </Descriptions.Item>
+            <Descriptions.Item key={key++} label="Address">
+              {`${patient.address[0].line[0]}, ${patient.address[0].city}, ${patient.address[0].state}, ${patient.address[0].country}`}
+            </Descriptions.Item>
+          </Descriptions>
+        )}
       </Drawer>
     );
   }
