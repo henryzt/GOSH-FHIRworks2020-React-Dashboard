ddd570df113c771b28347e7d776da01068408569
Henry Zhang <henryzhang9802@gmail.com>
2020-03-05 13:14:27 +0000

    Temporary solve rounting on mobile problem

--- src/components/MobileTabBar.js
+++ src/components/MobileTabBar.js
@@ -4,32 +4,29 @@ import "antd-mobile/dist/antd-mobile.css";
 
 import { HomeOutlined, TeamOutlined, SearchOutlined, BarChartOutlined } from "@ant-design/icons";
 
+import { withRouter } from "react-router-dom";
+
 const iconNonSelectStyle = { fontSize: "22px" };
 const iconSelectedStyle = { fontSize: "22px", color: "#33A3F4" };
 
 class TabBarMenu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      hidden: false,
-      fullScreen: false
-    };
   }
 
   render() {
+    const { location } = this.props;
     return (
-      <div style={{ position: "fixed", height: "100%", width: "100%", top: 0 }}>
+      <div style={{ position: "fixed", height: "100%", width: "100%", top: 0, zIndex: 100 }}>
         <TabBar unselectedTintColor="#949494" tintColor="#33A3F4" barTintColor="white">
           <TabBar.Item
             title="Home"
             key="Home"
             icon={<HomeOutlined style={iconNonSelectStyle} />}
             selectedIcon={<HomeOutlined style={iconSelectedStyle} />}
-            selected={this.state.selectedTab === "Home"}
+            selected={location.pathname === "/"}
             onPress={() => {
-              this.setState({
-                selectedTab: "Home"
-              });
+              window.location = "#/";
             }}
             data-seed="logId"
           ></TabBar.Item>
@@ -39,11 +36,9 @@ class TabBarMenu extends React.Component {
             key="Patients"
             icon={<TeamOutlined style={iconNonSelectStyle} />}
             selectedIcon={<TeamOutlined style={iconSelectedStyle} />}
-            selected={this.state.selectedTab === "Patients"}
+            selected={location.pathname === "patients"}
             onPress={() => {
-              this.setState({
-                selectedTab: "Patients"
-              });
+              window.location = "#/patients";
             }}
             data-seed="logId"
           ></TabBar.Item>
@@ -53,7 +48,7 @@ class TabBarMenu extends React.Component {
             key="Search"
             icon={<SearchOutlined style={iconNonSelectStyle} />}
             selectedIcon={<SearchOutlined style={iconSelectedStyle} />}
-            selected={this.state.selectedTab === "Search"}
+            selected={location.pathname === "search"}
             onPress={() => {
               this.setState({
                 selectedTab: "Search"
@@ -67,7 +62,7 @@ class TabBarMenu extends React.Component {
             key="Statistics"
             icon={<BarChartOutlined style={iconNonSelectStyle} />}
             selectedIcon={<BarChartOutlined style={iconSelectedStyle} />}
-            selected={this.state.selectedTab === "Statistics"}
+            selected={location.pathname === "statistics"}
             onPress={() => {
               this.setState({
                 selectedTab: "Statistics"
@@ -81,4 +76,4 @@ class TabBarMenu extends React.Component {
   }
 }
 
-export default TabBarMenu;
+export default withRouter(TabBarMenu);
