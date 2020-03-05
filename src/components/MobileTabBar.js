331c43243a54a3ccfd72e2bd681539614131a632
Henry Zhang <henryzhang9802@gmail.com>
2020-03-05 13:31:12 +0000

    Change back to object :/

--- src/components/MobileTabBar.js
+++ src/components/MobileTabBar.js
@@ -9,65 +9,68 @@ import { withRouter, useHistory } from "react-router-dom";
 const iconNonSelectStyle = { fontSize: "22px" };
 const iconSelectedStyle = { fontSize: "22px", color: "#33A3F4" };
 
-function TabBarMenu(props) {
-  let history = useHistory();
+class TabBarMenu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  const { location } = props;
-  console.log(location);
+  render() {
+    const { location, history } = this.props;
+    console.log(location, history);
+    return (
+      <div style={{ position: "fixed", height: "100%", width: "100%", top: 0, zIndex: 100 }}>
+        <TabBar unselectedTintColor="#949494" tintColor="#33A3F4" barTintColor="white">
+          <TabBar.Item
+            title="Home"
+            key="Home"
+            icon={<HomeOutlined style={iconNonSelectStyle} />}
+            selectedIcon={<HomeOutlined style={iconSelectedStyle} />}
+            selected={location.pathname === "/"}
+            onPress={() => {
+              history.push("/");
+            }}
+          ></TabBar.Item>
 
-  return (
-    <div style={{ position: "fixed", height: "100%", width: "100%", top: 0, zIndex: 100 }}>
-      <TabBar unselectedTintColor="#949494" tintColor="#33A3F4" barTintColor="white">
-        <TabBar.Item
-          title="Home"
-          key="Home"
-          icon={<HomeOutlined style={iconNonSelectStyle} />}
-          selectedIcon={<HomeOutlined style={iconSelectedStyle} />}
-          selected={location.pathname === "/"}
-          onPress={() => {
-            history.push("/");
-          }}
-        ></TabBar.Item>
+          <TabBar.Item
+            title="Patients"
+            key="Patients"
+            icon={<TeamOutlined style={iconNonSelectStyle} />}
+            selectedIcon={<TeamOutlined style={iconSelectedStyle} />}
+            selected={location.pathname === "/patients"}
+            onPress={() => {
+              history.push("/patients");
+            }}
+          ></TabBar.Item>
 
-        <TabBar.Item
-          title="Patients"
-          key="Patients"
-          icon={<TeamOutlined style={iconNonSelectStyle} />}
-          selectedIcon={<TeamOutlined style={iconSelectedStyle} />}
-          selected={location.pathname === "/patients"}
-          onPress={() => {
-            history.push("/patients");
-          }}
-        ></TabBar.Item>
+          <TabBar.Item
+            title="Search"
+            key="Search"
+            icon={<SearchOutlined style={iconNonSelectStyle} />}
+            selectedIcon={<SearchOutlined style={iconSelectedStyle} />}
+            selected={location.pathname === "/search"}
+            onPress={() => {
+              this.setState({
+                selectedTab: "Search"
+              });
+            }}
+          ></TabBar.Item>
 
-        <TabBar.Item
-          title="Search"
-          key="Search"
-          icon={<SearchOutlined style={iconNonSelectStyle} />}
-          selectedIcon={<SearchOutlined style={iconSelectedStyle} />}
-          selected={location.pathname === "/search"}
-          onPress={() => {
-            this.setState({
-              selectedTab: "Search"
-            });
-          }}
-        ></TabBar.Item>
-
-        <TabBar.Item
-          title="Statistics"
-          key="Statistics"
-          icon={<BarChartOutlined style={iconNonSelectStyle} />}
-          selectedIcon={<BarChartOutlined style={iconSelectedStyle} />}
-          selected={location.pathname === "/statistics"}
-          onPress={() => {
-            this.setState({
-              selectedTab: "Statistics"
-            });
-          }}
-        ></TabBar.Item>
-      </TabBar>
-    </div>
-  );
+          <TabBar.Item
+            title="Statistics"
+            key="Statistics"
+            icon={<BarChartOutlined style={iconNonSelectStyle} />}
+            selectedIcon={<BarChartOutlined style={iconSelectedStyle} />}
+            selected={location.pathname === "/statistics"}
+            onPress={() => {
+              this.setState({
+                selectedTab: "Statistics"
+              });
+            }}
+          ></TabBar.Item>
+        </TabBar>
+      </div>
+    );
+  }
 }
 
 export default withRouter(TabBarMenu);
