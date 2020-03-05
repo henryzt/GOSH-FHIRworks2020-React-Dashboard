da1dc2b51ed05f322951df753e67817e12505d9d
Henry Zhang <henryzhang9802@gmail.com>
2020-03-05 15:13:25 +0000

    Fix mobile header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = props => {
   const height = props.isHome ? "400px" : "200px";
   const lineHeight = props.isHome ? "330px" : "200px";
   return (
-    <div className="header" style={{ height, lineHeight }}>
+    <div className={props.isHome ? "header mobileHeader" : "header"} style={{ height, lineHeight }}>
       {props.title}
     </div>
   );
