73b10ed3a77209540e96e86a4f037f142f0b54b8
Henry Zhang <henryzhang9802@gmail.com>
2020-03-06 04:01:29 +0000

    Updates

---
+++ precache-manifest.ffce0b96506109fcb573bdb68b569386.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817548906a2c6abb3c5ff7fd756942cd",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/index.html"
+  },
+  {
+    "revision": "02122aa84d773baf799f",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/css/2.d2675d1f.chunk.css"
+  },
+  {
+    "revision": "89dcc86479bb1a224547",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/css/main.87e61db6.chunk.css"
+  },
+  {
+    "revision": "02122aa84d773baf799f",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/js/2.3f7be184.chunk.js"
+  },
+  {
+    "revision": "adbe8770dfe80185b1d2467350068428",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/js/2.3f7be184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89dcc86479bb1a224547",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/js/main.90339d94.chunk.js"
+  },
+  {
+    "revision": "ade74a47322b7b3f493b",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/js/runtime-main.c4c8e9ea.js"
+  },
+  {
+    "revision": "526965d0adc32f9e74862da495b34e46",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/media/fhir-logo-long.526965d0.png"
+  },
+  {
+    "revision": "15663ee699031399b0f8b71c3b33d0c3",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/media/homebg.15663ee6.jpg"
+  },
+  {
+    "revision": "b1161af5b91c1a4c1e3f9dccc1ecef62",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/media/icon_search.b1161af5.png"
+  },
+  {
+    "revision": "d19ad765b1148ec8f843638d37d04b2b",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/media/icon_stats.d19ad765.png"
+  },
+  {
+    "revision": "de1b14456a379252a044cecac653206e",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/media/icon_user.de1b1445.png"
+  },
+  {
+    "revision": "9d7bc07680bfa432ff6bdae90ef109cd",
+    "url": "/GOSH-FHIRworks2020-React-Dashboard/static/media/profile.9d7bc076.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/GOSH-FHIRworks2020-React-Dashboard/precache-manifest.ffce0b96506109fcb573bdb68b569386.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GOSH-FHIRworks2020-React-Dashboard/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
