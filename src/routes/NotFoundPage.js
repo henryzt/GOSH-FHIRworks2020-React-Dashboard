8099fe50f8a22b4a0c976ae9eb1b000cfc4089a3
Henry Zhang <henryzhang9802@gmail.com>
2020-03-06 13:35:36 +0000

    Add not found log

--- src/routes/NotFoundPage.js
+++ src/routes/NotFoundPage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import { Result, Button } from "antd";
+import { useLocation, useHistory, useRouteMatch } from "react-router-dom";
 
 const NotFoundPage = props => {
+  const location = useLocation();
+  const match = useRouteMatch("/");
+
+  console.log(location, match);
+
   return (
     <div>
       <Header title="404 Not Found"></Header>
