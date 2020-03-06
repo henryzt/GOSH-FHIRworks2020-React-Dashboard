be3ba550dc87ac833daa919e85ee7d6376eae1bd
Henry Zhang <henryzhang9802@gmail.com>
2020-03-06 20:40:29 +0000

    Add logo to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Header.css";
+import logo from "../img/fhir-dashboard-logo.png";
 
 const Header = props => {
   const height = props.isHome ? "400px" : "200px";
   const lineHeight = props.isHome ? "330px" : "200px";
   return (
     <div className={props.isHome ? "header mobileHeader" : "header"} style={{ height, lineHeight }}>
-      {props.title}
+      {props.isHome ? (
+        <img src={logo} style={{ width: "70%", maxWidth: "450px", height: "auto" }}></img>
+      ) : (
+        props.title
+      )}
     </div>
   );
 };
